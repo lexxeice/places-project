57979a952927849f4a30912e4e2df650b8b54131
BeOff <45692757+BeOff@users.noreply.github.com>
2019-06-19 19:12:51 +0300

    Fixed failing JS compressor (#40)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,9 +27,11 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
-  # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+  # Compress JavaScripts and CSS
+  # config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
+  # We use JS ES6
+  config.assets.js_compressor = Uglifier.new(harmony: true)
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   # config.assets.compile = false
